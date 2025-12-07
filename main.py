c93661f2496a84485956199099d2640fc0d878e6
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2025-12-07 22:40:03 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     # Initialize pygame
     pygame.init()
+    # A clock object
+    Clock = pygame.time.Clock()
+    # Delta variable
+    dt = 0
     # Use the display.set_mode function to get a new instance of GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -17,8 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # Refreshes the screen
         pygame.display.flip()
-
+        # Call the tick method with a value of 60 -> It pauses the game loop until 1/60th of a second has pased.
+        # The tick method also returns the amount of time passed since the last time it was called. 
+        # Values is divided by 1000 to convert milliseconds to seconds   
+        dt = Clock.tick(60) /1000
 if __name__ == "__main__":
     main()
 
